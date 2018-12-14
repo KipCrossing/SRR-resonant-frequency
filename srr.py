bbef008254cc68848a2a4e65ad678c40b0e79e61
KipCrossing <kip.crossing@gmail.com>
2018-12-14 16:01:15 +1100

    doc blocs added for class and methods

--- srr.py
+++ srr.py
@@ -3,40 +3,67 @@
 import math
 
 class SplitRing(object):
-    @property
-    def _permittivity(self):
-        return 8.854187817*(10**-12)   #E0
-    @property
-    def _permeability(self):
-        return math.pi*4*(10**-6)      #u0
+    """Described a split ring and its assosiated properties for split ring resinator (SRR) applications. inspired by the paper 'Analytical formulation for the resonant frequency of split rings' """
+
 
     def __init__(self,gap,height,width,radius):
+        """Short summary.
+
+        Parameters
+        ----------
+        gap : float
+            The size of the gap in the ring.
+        height : float
+            The height if the ring.
+        width : float
+            The width of the ring.
+        radius : float
+            the radius of the ring. From center to inner side of the ring.
+        """
         self.gap = gap
+
         self.height = height
         self.width = width
         self.radius = radius
 
+    @property
+    def permittivity(self):
+        """str: permittivity of free space"""
+        return 8.854187817*(10**-12)   #E0
+    @property
+    def permeability(self):
+        """str: permeability of free space"""
+        return math.pi*4*(10**-6)      #u0
+
     def Inductance(self):
+        """Returns:
+            float: the indictance of the SRR"""
         Rm = self.radius + (self.width/2)
-        L = Rm*self._permeability*(math.log(8*Rm/(self.height+self.width))-0.5)
+        L = Rm*self.permeability*(math.log(8*Rm/(self.height+self.width))-0.5)
         return L
 
     def C_gap(self):
-        Co = self._permittivity*(self.height+self.width+self.gap)
-        C = self._permittivity*(self.height*self.width/self.gap)+ Co
+        """Returns:
+            float: the gap capacitance of the SRR"""
+        Co = self.permittivity*(self.height+self.width+self.gap)
+        C = self.permittivity*(self.height*self.width/self.gap)+ Co
         return C
 
     def C_surf(self):
-        C = (2*self._permittivity*(self.height + self.width)/math.pi)*math.log(4*self.radius/self.gap)
+        """Returns:
+            float: the surface capacitance of the SRR"""
+        C = (2*self.permittivity*(self.height + self.width)/math.pi)*math.log(4*self.radius/self.gap)
         return C
 
     def Capacitance(self):
+        """Returns:
+            float: the total capacitance of the SRR"""
         return self.C_gap() + self.C_surf()
 
     def ResonantFrequency(self):
+        """Returns:
+            float: the resonant frequency of the SRR"""
         f = 1/(2*math.pi*math.sqrt(self.Inductance()*self.Capacitance()))
         return f
 
-SR = SplitRing(5,0.5,0.5,10)
-print(SR._permeability)
-print(SR.ResonantFrequency())
+help(SplitRing)
