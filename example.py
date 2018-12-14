623b1655ff4a53d0371a7447e29acd1ba0c5f4ec
KipCrossing <kip.crossing@gmail.com>
2018-12-14 17:46:39 +1100

    Clarify outputs in example.py

--- example.py
+++ example.py
@@ -5,6 +5,7 @@ SR = SplitRing(0.004,0.001,0.001,0.015) # Units in m
 RF = SR.ResonantFrequency()
 
 print('Resonant Frequency: %f GHz' % (RF/10**9))
+# Resonant Frequency: 2.043394 GHz
 
 # Other useful properties include:
 SR.permittivity     # permittivity of free space
